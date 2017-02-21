fb95600f561a88018a54ff039fbee7042c6c3f08
thamsy <thamsy94@gmail.com>
2017-02-22 00:01:42 +0800

    Add multible webhooks functionality

--- main.go
+++ main.go
@@ -50,10 +50,21 @@ func setUpLog() {
 }
 
 func postSlack(msg string) error {
+	var urls = []string{secret.SlackBot, secret.SlackBot2}
+	for _, url := range urls {
+		err := postSlackToURL(msg, url)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func postSlackToURL(msg string, url string) error {
 	var json = []byte(`{"username":"cs2103notifier",
     "icon_emoji": ":innocent:",
     "text":"` + msg + `"}`)
-	req, _ := http.NewRequest("POST", secret.SlackBot, bytes.NewBuffer(json))
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(json))
 	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
